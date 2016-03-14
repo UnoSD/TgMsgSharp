2254056f712535b5fd3a15d447b28d8d58002932
UnoSD <unosd.dev@gmail.com>
2016-03-14 17:52:20 +0000

    Adding support for parallel sends and message processing (incomplete).

---
+++ TgMsgSharp/Connector/ParallelMessagesProcessor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog;
+using TLSharp.Core;
+using TLSharp.Core.MTProto;
+
+namespace TgMsgSharp.Connector
+{
+    public class ParallelMessagesProcessor
+    {
+        volatile int _workingMessagesProcessors;
+
+        async Task<IReadOnlyCollection<TgMessage>> GetMessagesForContactId(TelegramClient _client, int contactId, Func<IEnumerable<Message>, IEnumerable<TgMessage>> map)
+        {
+            var returnValue = new ConcurrentBag<TgMessage>();
+
+            var messagesProcessor = new MessagesProcessor(_client, contactId);
+
+            var messagesReturned = new List<bool>();
+
+            // As long as all the processors are returning messages, we assume there are still messages to process.
+            while (messagesReturned.All(returned => returned))
+            {
+                if (_workingMessagesProcessors <= Environment.ProcessorCount)
+                {
+                    var processorsToStart = Environment.ProcessorCount - _workingMessagesProcessors;
+
+                    var sdf = new List<Task>();
+
+                    for (var index = 0; index <= processorsToStart; index++)
+                        sdf.Add(ProcessMessages(messagesProcessor, returnValue, map).ContinueWith(task =>
+                        {
+                            Interlocked.Decrement(ref _workingMessagesProcessors);
+                            messagesReturned.Add(task.Result);
+                        }));
+
+                    Interlocked.Add(ref _workingMessagesProcessors, processorsToStart);
+
+                    //await Task.WhenAny(sdf);
+                    await Task.WhenAll(sdf);
+                }
+            }
+
+            return returnValue.ToArray();
+        }
+
+        async Task<bool> ProcessMessages(MessagesProcessor messagesProcessor, ConcurrentBag<TgMessage> returnValue, Func<IEnumerable<Message>, IEnumerable<TgMessage>> map)
+        {
+            try
+            {
+                var messages = await messagesProcessor.GetMessages();
+
+                if (!messages.Any()) return false;
+
+                var tgMessages = map(messages);
+
+                foreach (var tgMessage in tgMessages)
+                    returnValue.Add(tgMessage);
+            }
+            catch (Exception exception)
+            {
+                LogManager.GetCurrentClassLogger().Error(exception);
+
+                return true;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ TgMsgSharp/TLSharp/Network/ParallelTcpTransport.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TLSharp.Core.Network
+{
+    public class ParallelTcpTransport : IDisposable
+    {
+        private readonly TcpClient _tcpClient;
+        private int sendCounter = 0;
+        readonly object _locker = new object();
+
+        public ParallelTcpTransport(string address, int port)
+        {
+            _tcpClient = new TcpClient();
+			
+            var ipAddress = IPAddress.Parse(address);
+            _tcpClient.Connect(ipAddress, port);
+        }
+
+        public void Send(byte[] packet)
+        {
+            if (!_tcpClient.Connected)
+                throw new InvalidOperationException("Client not connected to server.");
+
+            var tcpMessage = new TcpMessage(sendCounter, packet);
+
+            lock (_locker)
+            {
+                _tcpClient.GetStream().Write(tcpMessage.Encode(), 0, tcpMessage.Encode().Length);
+                sendCounter++;
+            }
+        }
+        
+        public TcpMessage Receieve()
+        {
+            lock (_locker)
+            {
+                var stream = _tcpClient.GetStream();
+
+                var packetLengthBytes = new byte[4];
+                if (stream.Read(packetLengthBytes, 0, 4) != 4)
+                    throw new InvalidOperationException("Couldn't read the packet length");
+                int packetLength = BitConverter.ToInt32(packetLengthBytes, 0);
+
+                var seqBytes = new byte[4];
+                if (stream.Read(seqBytes, 0, 4) != 4)
+                    throw new InvalidOperationException("Couldn't read the sequence");
+                int seq = BitConverter.ToInt32(seqBytes, 0);
+
+                int readBytes = 0;
+                var body = new byte[packetLength - 12];
+                int neededToRead = packetLength - 12;
+
+                do
+                {
+                    var bodyByte = new byte[packetLength - 12];
+                    var availableBytes = stream.Read(bodyByte, 0, neededToRead);
+                    neededToRead -= availableBytes;
+                    Buffer.BlockCopy(bodyByte, 0, body, readBytes, availableBytes);
+                    readBytes += availableBytes;
+                }
+                while (readBytes != packetLength - 12);
+
+                var crcBytes = new byte[4];
+                if (stream.Read(crcBytes, 0, 4) != 4)
+                    throw new InvalidOperationException("Couldn't read the crc");
+                int checksum = BitConverter.ToInt32(crcBytes, 0);
+
+                byte[] rv = new byte[packetLengthBytes.Length + seqBytes.Length + body.Length];
+
+                Buffer.BlockCopy(packetLengthBytes, 0, rv, 0, packetLengthBytes.Length);
+                Buffer.BlockCopy(seqBytes, 0, rv, packetLengthBytes.Length, seqBytes.Length);
+                Buffer.BlockCopy(body, 0, rv, packetLengthBytes.Length + seqBytes.Length, body.Length);
+                var crc32 = new Ionic.Crc.CRC32();
+                crc32.SlurpBlock(rv, 0, rv.Length);
+                var validChecksum = crc32.Crc32Result;
+
+                if (checksum != validChecksum)
+                {
+                    throw new InvalidOperationException("invalid checksum! skip");
+                }
+
+                return new TcpMessage(seq, body);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_tcpClient.Connected)
+                _tcpClient.Close();
+        }
+    }
+}
